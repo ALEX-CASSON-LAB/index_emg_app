7cf97b3facace746eda1d7dc8dfc3a8ed79ef64c
Carolina Costa Lopes <carolina.sofia.costa@hotmail.com>
2022-07-08 01:55:29 +0100

    Add exercise stats to database

--- DelsysAPI-XamarinAndroidExample/AndroidSample.Core/Session.cs
+++ DelsysAPI-XamarinAndroidExample/AndroidSample.Core/Session.cs
@@ -13,12 +13,29 @@ namespace AndroidSample.Core
         [MaxLength(8)]
         public DateTime date { get; set; } //date of the session
         private List<Exercise> _exercises = new List<Exercise>();
+        private Dictionary<string, List<double>> _exerciseStats = new Dictionary<string, List<double>>(); // exercise id, maximum percentage achieved in each rep
 
         public void addExercise(Exercise exer)
         {
             _exercises.Add(exer);
         }
+        public void addExerciseStat(string exerciseId, double maxPercent)
+        {
+            List<double> percents = new List<double>();
+
+            if (_exerciseStats.ContainsKey(exerciseId)){
+                percents = _exerciseStats[exerciseId];
+                percents.Add(maxPercent);
+                _exerciseStats[exerciseId] = percents;
+            }
+            else
+            {
+                percents.Add(maxPercent);
+                _exerciseStats.Add(exerciseId, percents);
+            }
+        }
     }
+
     [Table("Exercises")]
     public class Exercise
     {
